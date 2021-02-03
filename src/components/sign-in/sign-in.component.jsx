cda802cf640aade0cbfbb492369fb2961d9dac88
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-02-03 13:55:26 -0600

    add signin component to signin and signup page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import './signin.styles.scss';
+import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
     constructor(props){
@@ -56,3 +56,5 @@ class SignIn extends React.Component {
         );
     }
 }
+
+export default SignIn;

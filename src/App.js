a16a5e3c0f5379ff62a64c756400e88ed30b6487
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-02-03 12:48:35 -0600

    add sign in and sign up page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+
 
 const HatsPage = () => (
   <div>
@@ -20,6 +22,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+    <div>SignInAndSignUpPage</div>
+);
+
+export default SignInAndSignUpPage;

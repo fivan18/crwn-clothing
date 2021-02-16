1e787e2b0e8b5b5700b31cbd0950bdd18e824a9f
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-02-16 15:20:18 -0600

    add handlers to submit and chenge events

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -5,13 +5,54 @@ import CustomButton from '../custom-button/custom-button.component';
 
 import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
-import 'sign-up.styles.scss';
+import './sign-up.styles.scss';
 
 class SignUp extends React.Component {
     constructor(props){
         super(props);
+
+        this.state = {
+          displayName: '',
+          email: '',
+          password: '',
+          confirmPassword: ''
+        };
     }
     
+    handleSubmit = async event => {
+      event.preventDefault();
+  
+      const { displayName, email, password, confirmPassword } = this.state;
+  
+      if (password !== confirmPassword) {
+        alert("passwords don't match");
+        return;
+      }
+  
+      try {
+        const { user } = await auth.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+  
+        await createUserProfileDocument(user, { displayName });
+  
+        this.setState({
+          displayName: '',
+          email: '',
+          password: '',
+          confirmPassword: ''
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    handleChange = event => {
+      const { name, value } = event.target;
+  
+      this.setState({ [name]: value });
+    };
 
     render() {
         const { displayName, email, password, confirmPassword } = this.state;

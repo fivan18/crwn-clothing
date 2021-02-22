2f2b6e177e63b26ef7cfa32910b915147f6be609
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-02-22 16:04:10 -0600

    fix eslint errors

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,29 +7,32 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
+// eslint-disable-next-line react/prop-types
 const Header = ({ currentUser }) => (
-    <div className='header'>
-        <Link className='logo-container' to='/'>
-            <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
-                SHOP
-            </Link>
-            <Link className='option' to='/shop'>
-                CONTACT
-            </Link>
-            {currentUser ? (
-                <div className='option' onClick={() => auth.signOut()}>
-                    SIGN OUT
-                </div>
-            ) : (
-                <Link className='option' to='/signin'>
-                    SIGN IN
-                </Link>
-            )}
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        // eslint-disable-next-line max-len
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
     </div>
+  </div>
 );
 
 export default Header;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,22 +3,33 @@ import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
-    <div 
-        className={`${size} menu-item`}
-        onClick={() => history.push(`${match.url}${linkUrl}`)}
-    >
-        <div
-            className='background-image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>SHOP NOW</span>
-        </div>
+const MenuItem = ({
+  // eslint-disable-next-line react/prop-types
+  title, imageUrl, size, linkUrl, history, match,
+}) => (
+  // eslint-disable-next-line max-len
+  // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+  <div
+    className={`${size} menu-item`}
+    // eslint-disable-next-line react/prop-types
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="content">
+      <h1 className="title">
+        {
+            // eslint-disable-next-line react/prop-types
+            title.toUpperCase()
+        }
+      </h1>
+      <span className="subtitle">SHOP NOW</span>
     </div>
+  </div>
 );
 
-export default withRouter(MenuItem);    
+export default withRouter(MenuItem);

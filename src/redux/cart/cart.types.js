860ab33438d4ce97becafef74855639a9afd7ebd
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-02-26 14:23:21 -0600

    combine cart reducer

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,5 @@
 const CartActionTypes = {
-    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
-  };
-  
-  export default CartActionTypes;
+  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+};
+
+export default CartActionTypes;

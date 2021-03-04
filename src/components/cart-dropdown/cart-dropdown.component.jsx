260ddbb6301f9e646cc2fd9801b7f092f478aefc
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-03-04 15:55:44 -0600

    count items added to the cart and add selectors to he app

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import PropTypes from 'prop-types';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
@@ -31,8 +32,8 @@ CartDropdown.propTypes = {
   ).isRequired,
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);

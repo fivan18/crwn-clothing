7237de7992adf7e3dbe8c66a0794181e3ce0711d
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-03-05 14:19:23 -0600

    increment and decrement items from the cart

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -4,11 +4,17 @@ import { connect } from 'react-redux';
 
 import PropTypes from 'prop-types';
 
-import { clearItemFromCart } from '../../redux/cart/cart.actions';
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({
+  cartItem, clearItem, addItem, removeItem,
+}) => {
   const {
     name, imageUrl, price, quantity,
   } = cartItem;
@@ -18,7 +24,15 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>
         &#10005;
@@ -35,10 +49,14 @@ CheckoutItem.propTypes = {
     quantity: PropTypes.number.isRequired,
   }).isRequired,
   clearItem: PropTypes.func.isRequired,
+  addItem: PropTypes.func.isRequired,
+  removeItem: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = dispatch => ({
   clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item)),
 });
 
 export default connect(

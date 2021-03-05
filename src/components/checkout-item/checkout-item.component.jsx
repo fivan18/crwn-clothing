d739b5db87297514c94073871090c401c54204b6
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-03-05 13:48:20 -0600

    delete checkout item

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,24 +1,31 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */
 import React from 'react';
+import { connect } from 'react-redux';
 
 import PropTypes from 'prop-types';
 
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
+
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({
-  cartItem: {
+const CheckoutItem = ({ cartItem, clearItem }) => {
+  const {
     name, imageUrl, price, quantity,
-  },
-}) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item" />
+  } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-button">&#10005;</div>
-  </div>
-);
+  );
+};
 
 CheckoutItem.propTypes = {
   cartItem: PropTypes.shape({
@@ -27,6 +34,14 @@ CheckoutItem.propTypes = {
     imageUrl: PropTypes.string.isRequired,
     quantity: PropTypes.number.isRequired,
   }).isRequired,
+  clearItem: PropTypes.func.isRequired,
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item)),
+});
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(CheckoutItem);
